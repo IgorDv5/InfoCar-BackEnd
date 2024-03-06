44f34d78b4ec19af06e39bdfa951bde3783fd04b
IgorDv5 <igoraluno2005@gmail.com>
2024-03-05 22:02:34 -0300

    Projeto Iniciado

---
+++ src/test/java/com/igor/car/CarApplicationTests.java
@@ -0,0 +1,13 @@
+package com.igor.car;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CarApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
