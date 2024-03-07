15fddd379994488a0ec0e4aeda0611ad55caea09
IgorDv5 <igoraluno2005@gmail.com>
2024-03-07 20:00:58 -0300

    End Point Delete Para Pessoa Implementado

--- src/main/java/com/igor/car/services/PessoaService.java
+++ src/main/java/com/igor/car/services/PessoaService.java
@@ -14,17 +14,15 @@ import com.igor.car.repositories.PessoaRepository;
 import com.igor.car.services.exceptions.DataIntegrityViolationException;
 import com.igor.car.services.exceptions.ObjectNotFoundException;
 
-
-
 @Service
 public class PessoaService {
-	
+
 	@Autowired
 	private PessoaRepository repository;
-	
+
 	public Pessoa findById(Integer id) {
 		Optional<Pessoa> obj = repository.findById(id);
-		return obj.orElseThrow(() -> new ObjectNotFoundException("Objeto Não Encontrado! Id: "+ id));
+		return obj.orElseThrow(() -> new ObjectNotFoundException("Objeto Não Encontrado! Id: " + id));
 	}
 
 	public List<Pessoa> findAll() {
@@ -34,10 +32,10 @@ public class PessoaService {
 	public Pessoa create(PessoaDTO objDTO) {
 		objDTO.setId(null);
 		validaPorCpfeEmail(objDTO);
-		Pessoa newObj = new Pessoa(objDTO); 
+		Pessoa newObj = new Pessoa(objDTO);
 		return repository.save(newObj);
 	}
-	
+
 	public Pessoa update(Integer id, @Valid PessoaDTO objDTO) {
 		objDTO.setId(id);
 		Pessoa oldObj = findById(id);
@@ -46,18 +44,21 @@ public class PessoaService {
 		return repository.save(oldObj);
 	}
 
+	public void delete(Integer id) {
+		repository.deleteById(id);
+	}
+
 	private void validaPorCpfeEmail(PessoaDTO objDTO) {
 		Optional<Pessoa> obj = repository.findByCpf(objDTO.getCpf());
-			if(obj.isPresent() && obj.get().getId() != objDTO.getId()) {
-				throw new DataIntegrityViolationException("CPF já Cadastrado no Sistema!");
-			}
-			
-			obj = repository.findByEmail(objDTO.getEmail());
-			if(obj.isPresent() && obj.get().getId() != objDTO.getId()) {
-				throw new DataIntegrityViolationException("EMAIL já Cadastrado no Sistema!");
-			}
-			
+		if (obj.isPresent() && obj.get().getId() != objDTO.getId()) {
+			throw new DataIntegrityViolationException("CPF já Cadastrado no Sistema!");
+		}
+
+		obj = repository.findByEmail(objDTO.getEmail());
+		if (obj.isPresent() && obj.get().getId() != objDTO.getId()) {
+			throw new DataIntegrityViolationException("EMAIL já Cadastrado no Sistema!");
+		}
 
 	}
-	
+
 }
