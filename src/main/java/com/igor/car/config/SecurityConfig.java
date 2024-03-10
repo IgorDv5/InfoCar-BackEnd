6b6c0ed0ff02c4f05c330f3b65a7d54a08938127
IgorDv5 <igoraluno2005@gmail.com>
2024-03-10 13:47:03 -0300

    Autorização com JWT

--- src/main/java/com/igor/car/config/SecurityConfig.java
+++ src/main/java/com/igor/car/config/SecurityConfig.java
@@ -18,6 +18,7 @@ import org.springframework.web.cors.CorsConfigurationSource;
 import org.springframework.web.cors.UrlBasedCorsConfigurationSource;
 
 import com.igor.car.security.JWTAuthenticationFilter;
+import com.igor.car.security.JWTAuthorizationFilter;
 import com.igor.car.security.JWTUtil;
 
 
@@ -42,7 +43,7 @@ public class SecurityConfig extends WebSecurityConfigurerAdapter {
 
 		http.cors().and().csrf().disable();
 		http.addFilter(new JWTAuthenticationFilter(authenticationManager(), jwtUtil));
-	
+		http.addFilter(new JWTAuthorizationFilter(authenticationManager(), jwtUtil, userDetailsService));
 		http.authorizeRequests().antMatchers(PUBLIC_MATCHERS).permitAll().anyRequest().authenticated();
 
 		http.sessionManagement().sessionCreationPolicy(SessionCreationPolicy.STATELESS);
